5e170163aa46140711322789055ca66a52225af6
Joel <pratt.joel@gmail.com>
2017-12-13 09:34:41 -0500

    Project setup

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+
+export default class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBS012kYgoyxm6ig-ldjhVUElcPldg1wE4',
+      authDomain: 'communityconnect-aa662.firebaseapp.com',
+      databaseURL: 'https://communityconnect-aa662.firebaseio.com',
+      projectId: 'communityconnect-aa662',
+      storageBucket: '',
+      messagingSenderId: '399029413772'
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text style={{ paddingTop: 20 }}>Hello World</Text>
+        </View>
+      </Provider>
+    );
+  }
+}

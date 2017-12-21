0c96a2857257335776c2ef269efc37e4ed56ba35
Joel <pratt.joel@gmail.com>
2017-12-21 00:01:25 -0500

    Implement fbLoginComplete helper method for Facebook Login.

--- src/components/FBLoginButton.js
+++ src/components/FBLoginButton.js
@@ -1,3 +1,5 @@
+'use-strict';
+
 import React, { Component } from 'react';
 import {
   LoginButton,
@@ -5,27 +7,28 @@ import {
 } from 'react-native-fbsdk';
 
 export default class FBLoginButton extends Component {
+  _fbLoginComplete = (error, result) => {
+    console.log('login finished called');
+    if (error) {
+      console.log(`Login failed with error: ${result.error}`);
+    } else if (result.isCancelled) {
+      console.log('Login was cancelled');
+    } else {
+      console.log(`Login was successful with permissions: ${result.grantedPermissions}`);
+      AccessToken.getCurrentAccessToken().then(
+        (data) => {
+          const token = data.accessToken.toString();
+          console.log(`User token is: ${token}`);
+        }
+      );
+    }
+  };
+
   render() {
     return (
       <LoginButton
         readPermissions={['public_profile']}
-        onLoginFinished={
-          (error, result) => {
-            if (error) {
-              console.log(`Login failed with error: ${result.error}`);
-            } else if (result.isCancelled) {
-              console.log('Login was cancelled');
-            } else {
-              console.log(`Login was successful with permissions: ${result.grantedPermissions}`);
-              AccessToken.getCurrentAccessToken().then(
-                (data) => {
-                  const token = data.accessToken.toString();
-                  console.log(`User token is: ${token}`);
-                }
-              );
-            }
-          }
-        }
+        onLoginFinished={this._fbLoginComplete}
         onLogoutFinished={() => console.log('User logged out')}
       />
     );

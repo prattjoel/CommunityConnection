ee1d7343c20a9cd847d228c78035fef16378feca
Joel Pratt <pratt.joel@gmail.com>
2018-01-18 17:49:23 -0500

    Add ChatRoom reducer for chat room state.

---
+++ src/reducers/ChatRoomReducer.js
@@ -0,0 +1,19 @@
+'use strict';
+import {
+  GET_CHAT_ROOM,
+  SET_CHAT_ROOM,
+  GENERAL_CHAT_ROOM,
+} from '../actions/types';
+
+const initialState = {
+  currentChatRoom: GENERAL_CHAT_ROOM
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SET_CHAT_ROOM:
+      return { ...state, currentChatRoom: action.payload };
+    default:
+      return (state);
+  }
+};

599994e9a385476b97826828a60a4996e06e06b1
Joel Pratt <pratt.joel@gmail.com>
2018-01-24 00:45:25 -0500

    Remove double nav bar from nested StackNavigator

--- src/Router.js
+++ src/Router.js
@@ -1,26 +1,44 @@
 'use strict';
 
 import React from 'react';
-import { StackNavigator } from 'react-navigation';
+import { StackNavigator, TabNavigator } from 'react-navigation';
 import Login from './components/Login';
 import Home from './components/Home';
 import ChatRooms from './containers/ChatRooms';
 // import MenuImage from './assets/menu_burger.png';
 import ChatMenuButton from './components/ChatMenuButton';
 
-const MainNav = StackNavigator({
-  Login: {
-    screen: Login,
-    navigationOptions: {
-      headerTitle: 'Login'
-    }
+const MessageNav = StackNavigator(
+
+{
+   MessageHome: {
+      screen: Home,
+      navigationOptions: {
+        headerTitle: 'Messages'
+      },
+    },
   },
-  Home: {
-    screen: Home,
-    navigationOptions: {
-      headerTitle: 'Messages'
+  {
+    headerMode: 'none'
+  }
+);
+
+const MainNav = StackNavigator(
+  {
+    Login: {
+      screen: Login,
+      navigationOptions: {
+        headerTitle: 'Login'
+      }
+    },
+    Home: {
+      screen: MessageNav,
+      headerMode: 'none'
+      // navigationOptions: {
+      //   headerMode: 'none'
+      // }
     }
   }
-});
+);
 
 export default MainNav;

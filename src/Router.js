8150b4a75aed24a6ce39a02754694a2dcdefbbdd
Joel Pratt <pratt.joel@gmail.com>
2018-01-26 14:37:25 -0500

    Add menu drawer with React Navigation.

--- src/Router.js
+++ src/Router.js
@@ -1,25 +1,29 @@
 'use strict';
 
 import React from 'react';
-import { StackNavigator, TabNavigator } from 'react-navigation';
+import { Text } from 'react-native';
+import { StackNavigator, TabNavigator, DrawerNavigator } from 'react-navigation';
 import Login from './components/Login';
 import Home from './components/Home';
 import ChatRooms from './containers/ChatRooms';
 import GivingPage from './components/Giving';
 import ContactForm from './components/ContactForm';
 import Announcements from './components/Announcements';
+import SettingsMenu from './components/Settings';
 
 // import MenuImage from './assets/menu_burger.png';
 // import ChatMenuButton from './components/ChatMenuButton';
 // import Messages from './components/Messages';
 
-const MessagesNav = TabNavigator(
+
+
+const HomeNav = TabNavigator(
   {
     ChatroomChooser: {
       screen: ChatRooms,
-      navigationOptions: {
-        headerTitle: 'Messages'
-      },
+      // navigationOptions: {
+      //   headerTitle: 'Messages'
+      // },
     },
     GivingPage: {
       screen: GivingPage
@@ -33,38 +37,76 @@ const MessagesNav = TabNavigator(
   },
 );
 
-const HomeNav = StackNavigator(
+const MessagesNav = StackNavigator(
 
 {
-   MessageNav: {
-      screen: MessagesNav,
-      navigationOptions: {
-        headerTitle: 'Messages'
-      },
+   HomeTabs: {
+      screen: HomeNav,
+      // navigationOptions: {
+      //   headerTitle: 'Messages Home'
+      // },
     },
     Messages: {
       screen: Home,
-      navigationOptions: {
-        headerTitle: 'Current Messages'
-      },
-    }
+      // navigationOptions: {
+      //   headerTitle: 'Current Messages'
+      // },
+    },
   },
   {
     headerMode: 'none'
   }
 );
 
+const MenuDrawer = DrawerNavigator({
+  MessageHome: {
+    screen: MessagesNav
+  },
+  SettingsMenu: {
+    screen: SettingsMenu
+  },
+  Settings2: {
+    screen: SettingsMenu
+  },
+});
+
+const DrawerNav = StackNavigator(
+  {
+    MenuDrawer: {
+      screen: MenuDrawer
+    },
+},
+{
+  // initialRouteName: 'MessageHome',
+  headerMode: 'float',
+  navigationOptions: ({ navigation }) => ({
+    // headerStyle: { backgroundColor: '#4C3E54' },
+    title: 'Home',
+    // headerTintColor: 'white',
+    headerLeft:
+      <Text
+        onPress={() => {
+          navigation.navigate('DrawerToggle');
+        }
+        }
+      >
+        Menu
+      </Text>
+  })
+}
+);
+
 const MainNav = StackNavigator(
   {
     Login: {
       screen: Login,
-      navigationOptions: {
-        headerTitle: 'Login'
-      }
     },
     Home: {
-      screen: HomeNav,
-    }
+      screen: DrawerNav,
+    },
+  },
+  {
+    headerMode: 'none'
   }
 );
 

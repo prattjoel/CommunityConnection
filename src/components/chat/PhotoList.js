7b8e79c689062425118594095c39a3eef1bb3e3e
Joel Pratt <pratt.joel@gmail.com>
2018-03-03 16:20:07 -0500

    Enable unselection of images from PhotoList.

--- src/components/chat/PhotoList.js
+++ src/components/chat/PhotoList.js
@@ -11,16 +11,19 @@ import {
 export default class PhotoList extends Component {
 
     componentWillReceiveProps() {
-            console.log('selected images after selection made');
-            console.log(this.props.selectedImages);
+        console.log('selected images after selection made');
+        console.log(this.props.selectedImages);
     }
 
     onPress = (item) => {
         console.log('image pressed');
         console.log('selected images onPress');
         console.log(this.props.selectedImages);
-        // debugger;
-        const imageInfo = { uri: item.node.image.uri, filename: item.node.image.filename };
+        const imageInfo = {
+            uri: item.node.image.uri,
+            filename: item.node.image.filename
+        };
+
         this.props.selectImageToSend(imageInfo);
     }
 
@@ -38,40 +41,40 @@ export default class PhotoList extends Component {
                 onPress={this.onPress.bind(this, item)}
                 // activeOpacity={0.5}
             >
-                <Image
-                    source={{ uri: item.node.image.uri }}
-                    style={styles.imageStyle}
+                    <Image
+                        source={{ uri: item.node.image.uri }}
+                        style={styles.imageStyle}
 
-                    // style={{ ...styles.imageStyle, opacity: this.highlightImage() }}
-                />
-            </TouchableHighlight>
-        );
+                        // style={{ ...styles.imageStyle, opacity: this.highlightImage() }}
+                    />
+                </TouchableHighlight>
+            );
+        }
+
+        render() {
+            return (
+                <View style={{ flex: 10, paddingTop: 20 }}>
+                    <FlatList
+                        // style={{ flexDirection: 'column', borderWidth: 5, borderColor: 'black' }}
+                        data={this.props.currentImages}
+                        renderItem={this.renderItem}
+                        keyExtractor={this.keyExtractor}
+                        numColumns={3}
+                        // horizontal
+                    />
+                </View>
+            );
+        }
     }
 
-    render() {
-        return (
-            <View style={{ flex: 10, paddingTop: 20 }}>
-                <FlatList
-                    // style={{ flexDirection: 'column', borderWidth: 5, borderColor: 'black' }}
-                    data={this.props.currentImages}
-                    renderItem={this.renderItem}
-                    keyExtractor={this.keyExtractor}
-                    numColumns={3}
-                    // horizontal
-                />
-            </View>
-        );
-    }
-}
-
-const styles = {
-    imageStyle: {
-        width: 100,
-        height: 100,
-        marginLeft: 2,
-        marginTop: 2,
-        // borderRadius: 5,
-        // borderWidth: 1,
-        // borderColor: '#979797'
-    }
-};
+    const styles = {
+        imageStyle: {
+            width: 100,
+            height: 100,
+            marginLeft: 2,
+            marginTop: 2,
+            // borderRadius: 5,
+            // borderWidth: 1,
+            // borderColor: '#979797'
+        }
+    };
--- src/reducers/ImageReducer.js
+++ src/reducers/ImageReducer.js
@@ -3,7 +3,8 @@
 import {
     SET_CURRENT_IMAGES,
     SHOW_IMAGE_SELECTOR,
-    SELECT_IMAGE_TO_SEND
+    SELECT_IMAGE_TO_SEND,
+    // UNSELECT_IMAGE_TO_SEND
 } from '../constants/ImageTypes';
 
 const initialState = {
@@ -16,19 +17,26 @@ export default (state = initialState, action) => {
     // debugger;
     switch (action.type) {
         case SET_CURRENT_IMAGES:
-            return { ...state, currentImages: action.payload };
+        return { ...state, currentImages: action.payload };
         case SHOW_IMAGE_SELECTOR:
-            // console.log('showImages');
-            // console.log(action.payload);
-            return { ...state, showImages: action.payload };
+        // console.log('showImages');
+        // console.log(action.payload);
+        return { ...state, showImages: action.payload };
         case SELECT_IMAGE_TO_SEND:
         // debugger;
-            {
-                const selectedImages = state.selectedImages;
-                selectedImages.push(action.payload);
-            return { ...state, selectedImages };
+        {
+            const selectedImages = state.selectedImages;
+            const index = selectedImages.map((image) => image.filename)
+            .indexOf(action.payload.filename);
+            if (index !== -1) {
+                const currentSelectedImages = selectedImages.splice(index, 1);
+                return { ...state, currentSelectedImages };
             }
+            selectedImages.push(action.payload);
+            return { ...state, selectedImages };
+        }
+
         default:
-            return state;
+        return state;
     }
 };
